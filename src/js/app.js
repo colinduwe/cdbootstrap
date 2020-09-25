d4a5e55d29211068ae8dc3343be78c0ecfd10e23
Colin Duwe <colinduwe@gmail.com>
2020-09-25 14:47:25 -0700

    update Bootstrap version and modified app.js load only specific pieces of bootstrap js

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,17 @@
-import 'bootstrap';
+// Import all of the Bootstrap JS
+//import 'bootstrap';
+// Or just load the pieces you need
+import 'bootstrap/js/src/util';
+//import 'bootstrap/js/src/alert';
+//import 'bootstrap/js/src/button';
+//import 'bootstrap/js/src/carousel';
+//import 'bootstrap/js/src/collapse';
+//import 'bootstrap/js/src/dropdown';
+//import 'bootstrap/js/src/modal';
+//import 'bootstrap/js/src/popover';
+//import 'bootstrap/js/src/scrollspy';
+//import 'bootstrap/js/src/tab';
+////import 'bootstrap/js/src/toast';
+//import 'bootstrap/js/src/tooltip';
+
 import './skip-link-focus-fix.js';
